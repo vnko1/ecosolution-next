4b54546f04fdeec5fcf06646621a8fece1348b5d
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-24 17:54:51 +0300

    save

---
+++ src/app/_components/ContactUs/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { getValuesFromLS, removeDataFromLS } from "@/utils";
+import { IconEnum } from "@/types";
+import { UIButton } from "@/components";
+import { ContactsFormValue } from "./ContactForm.type";
+import { validationSchema } from "./validationSchema";
+import styles from "./ContactForm.module.scss";
+
+const formsData = {
+  fullName: { label: "* Full name:", placeholder: "John Rosie" },
+  email: { label: "* E-mail:", placeholder: "johnrosie@gmail.com" },
+  phone: { label: "* Phone:", placeholder: "380961234567" },
+  message: { label: "Message:", placeholder: "Your message" },
+};
+
+const initialValues = {
+  fullName: "",
+  email: "",
+  phone: "",
+  message: "",
+};
+
+const keyValues = Object.keys(initialValues);
+
+const ContactForm: FC = () => {
+  const [values] = useState(
+    getValuesFromLS<ContactsFormValue>(
+      initialValues,
+      "fullName",
+      "email",
+      "phone",
+      "message"
+    )
+  );
+  const [showUMessage, setShowUMessage] = useState(false);
+
+  const onHandleSubmit = (
+    values: ContactsFormValue,
+    { resetForm }: FormikHelpers<ContactsFormValue>
+  ) => {
+    const trimmedValue = Object.keys(values).reduce(
+      (acc, el) => ({
+        [el]: values[el as keyof ContactsFormValue].trim(),
+        ...acc,
+      }),
+      {}
+    );
+
+    console.log(trimmedValue);
+    setShowUMessage(true);
+    removeDataFromLS("fullName", "email", "phone", "message");
+    resetForm({ values: initialValues });
+    setTimeout(() => {
+      setShowUMessage(false);
+    }, 2000);
+  };
+
+  return (
+    <div className={styles["contact-form"]}>
+      <Formik
+        initialValues={values}
+        onSubmit={onHandleSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ isValid, values }) => {
+          return (
+            <Form className={styles["form"]}>
+              <div className={styles["form__field"]}>
+                {keyValues.map((key, i) => {
+                  if (key === "message")
+                    return (
+                      <TextField
+                        key={i}
+                        id={key}
+                        name={key}
+                        value={values[key]}
+                        label={formsData[key].label}
+                        placeholder={formsData[key].placeholder}
+                        component="textarea"
+                      />
+                    );
+
+                  return (
+                    <TextField
+                      key={i}
+                      id={key}
+                      name={key}
+                      value={values[key as keyof ContactsFormValue]}
+                      label={formsData[key as keyof ContactsFormValue].label}
+                      placeholder={
+                        formsData[key as keyof ContactsFormValue].placeholder
+                      }
+                    />
+                  );
+                })}
+              </div>
+              <div className={styles["form__wrapper"]}>
+                <UIButton
+                  variant="outlined"
+                  type="submit"
+                  icon={IconEnum.ARROW}
+                  iconClassNames={styles["icon"]}
+                  iconSize={16}
+                  disabled={!isValid}
+                  classNames={styles["button"]}
+                >
+                  Send
+                </UIButton>
+                {showUMessage ? (
+                  <p className={styles["message"]}>Your message was sent</p>
+                ) : null}
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/app/_components/ContactUs/components/Contacts/Contacts.tsx
@@ -0,0 +1,86 @@
+import { FC } from "react";
+
+import { IconEnum } from "@/types";
+import { NavLink } from "@/components";
+import styles from "./Contacts.module.scss";
+
+const Contacts: FC = () => {
+  return (
+    <div className={styles["contacts"]}>
+      <div>
+        <p className={styles["label"]}>Phone</p>
+        <ul className={styles["links"]}>
+          <li>
+            <NavLink
+              aria-label="Phone"
+              link="tel:+380931234567"
+              icon={IconEnum.CALL}
+              size={24}
+            >
+              38 (093) 12 34 567
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              aria-label="Phone"
+              link="tel:+380931234567"
+              icon={IconEnum.CALL}
+              size={24}
+            >
+              38 (093) 12 34 567
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p className={styles["label"]}>E-mail:</p>
+        <div className={styles["links"]}>
+          <NavLink
+            aria-label="Mail"
+            link="mailto:office@ecosolution.com"
+            icon={IconEnum.SMS}
+            size={24}
+          >
+            office@ecosolution.com
+          </NavLink>
+        </div>
+      </div>
+      <div>
+        <p className={styles["label"]}>Address:</p>
+        <div className={styles["links"]}>
+          <NavLink
+            aria-label="Location"
+            link="https://maps.app.goo.gl/UTyEsA13qhB9x7LTA"
+            icon={IconEnum.CALL}
+            size={24}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
+          </NavLink>
+        </div>
+      </div>
+      <div>
+        <p className={styles["label"]}>Social Networks:</p>
+        <div className={`${styles["links"]} ${styles["soc"]}`}>
+          <NavLink
+            aria-label="Facebook"
+            icon={IconEnum.FACEBOOK}
+            size={24}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          />
+          <NavLink
+            aria-label="Instagram"
+            icon={IconEnum.INSTAGRAM}
+            size={24}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
